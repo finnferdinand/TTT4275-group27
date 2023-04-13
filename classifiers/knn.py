943f7120205383c39140c8336c7ed3cc192e78dd
Finn Ferdinand Sandvand <ffsandva@stud.ntnu.no>
2023-04-13 18:45:46 +0200

    testing with 1nn classifier

--- classifiers/knn.py
+++ classifiers/knn.py
@@ -1,7 +1,6 @@
 import numpy as np
 import scipy.io
 import scipy.spatial
-from matplotlib import pyplot as plt
 
 # IMPORT DATASET
 MODULE_NAME = '.'.join(__name__.split('.')[:-1])
@@ -13,36 +12,47 @@ trainlab = data_all['trainlab'] # training labels
 testv    = data_all['testv']    # test data
 testlab  = data_all['testlab']  # test labels
 
-num_train = data_all['num_train'][0][0] # number of training samples
-num_test  = data_all['num_test'][0][0]  # number of testing samples
-row_size  = data_all['row_size'][0][0]  # number of rows in a single image
-col_size  = data_all['col_size'][0][0]  # number of columns in a single image
-vec_size  = data_all['vec_size'][0][0]  # number of elements in an image vector
+num_train = data_all['num_train'][0,0] # number of training samples
+num_test  = data_all['num_test'][0,0]  # number of testing samples
+row_size  = data_all['row_size'][0,0]  # number of rows in a single image
+col_size  = data_all['col_size'][0,0]  # number of columns in a single image
+vec_size  = data_all['vec_size'][0,0]  # number of elements in an image vector
 
-def test_1nn():
-    # CLASSIFY AN IMAGE
-    test_index = 0 # only testing first image for now, preliminary testing
-    test_image = testv[test_index:test_index+1, 0:vec_size] # select first image from test set
+def classifier_1nn():
+    confusion_matrix = np.zeros([10, 10]) # row = true class, col = classified class
+    SELECTION_SIZE = 10
+    misclassified_counter = 0
+    misclassified_selection = np.zeros([SELECTION_SIZE, vec_size])
+    misclassified_labels = []
+    correctly_classified_counter = 0
+    correctly_classified_selection = np.zeros([SELECTION_SIZE, vec_size])
+    correctly_classified_labels = []
 
     # Iterating through chunks of CHUNK_SIZE images from the training set
-    CHUNK_SIZE = num_train             # full training set only for preliminary testing (TODO: Set to 1000)
-    n_chunks = num_train // CHUNK_SIZE # number of training samples in a single chunk
-    for chunk in range(n_chunks):
-        chunk_data = trainv[CHUNK_SIZE*chunk : CHUNK_SIZE*(chunk+1), 0:vec_size] # chunk slice from full training set
-        dist = scipy.spatial.distance_matrix(chunk_data, test_image)             # distance vector from test image to chunk
-        nn_index = np.argmin(dist)                                               # find nearest neigbor -> smallest distance to test sample
-        print(f"Closest template {nn_index} " 
-            f"labeled {trainlab[nn_index+chunk*CHUNK_SIZE][0]} "
-            f"with a distance of {round(dist[nn_index][0], 2)}")
-        print("Actual label", testlab[test_index][0])
-
-    # DRAWING IMAGES
-    plt.subplot(1,2,1) # test sample image
-    square_test_image = np.reshape(test_image, [row_size, col_size])
-    plt.imshow(square_test_image, interpolation='nearest')
-    plt.title("Test Sample")
-    plt.subplot(1,2,2) # nearest neigbor image
-    square_closest_neighbor = np.reshape(chunk_data[nn_index:nn_index+1, 0:vec_size], [row_size, col_size])
-    plt.imshow(square_closest_neighbor, interpolation='nearest')
-    plt.title("Nearest Neighbor")
-    plt.show()
+    TRAINING_CHUNK_SIZE = 1000                    # full training set only for preliminary testing (TODO: Set to 1000)
+    NUM_CHUNKS = num_train // TRAINING_CHUNK_SIZE # number of training samples in a single chunk
+    TEST_CHUNK_SIZE = num_test // NUM_CHUNKS
+    for chunk in range(NUM_CHUNKS):
+        print(f"testing chunk: {chunk}")
+        training_chunk = trainv[TRAINING_CHUNK_SIZE*chunk:TRAINING_CHUNK_SIZE*(chunk+1), 0:vec_size] # chunk slice from full training set
+        test_chunk = testv[TEST_CHUNK_SIZE*chunk:TEST_CHUNK_SIZE*(chunk+1), 0:vec_size]
+        dist = scipy.spatial.distance_matrix(training_chunk, test_chunk)                             # distance vector from test image to chunk
+        for test_sample in range(TEST_CHUNK_SIZE):
+            nn_index = np.argmin(dist[test_sample,0:vec_size])
+            classified_label = trainlab[nn_index+chunk*TRAINING_CHUNK_SIZE,0]
+            actual_label = testlab[test_sample+chunk*TEST_CHUNK_SIZE,0]
+            confusion_matrix[actual_label, classified_label] += 1
+            if misclassified_counter < SELECTION_SIZE and classified_label != actual_label:
+                misclassified_selection[misclassified_counter] = testv[test_sample+chunk*TEST_CHUNK_SIZE, 0:vec_size]
+                misclassified_counter += 1
+                misclassified_labels.append(classified_label)
+            elif correctly_classified_counter < SELECTION_SIZE and classified_label == actual_label:
+                correctly_classified_selection[correctly_classified_counter] = testv[test_sample+chunk*TEST_CHUNK_SIZE, 0:vec_size]
+                correctly_classified_counter += 1
+                correctly_classified_labels.append(classified_label)
+        
+        if chunk > 5: break # ONLY FOR TESTING 
+    
+    misclassified = (misclassified_selection, misclassified_labels)
+    correctly_classified = (correctly_classified_selection, correctly_classified_labels)
+    return (confusion_matrix, misclassified, correctly_classified)
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import classifiers.knn
+import classifiers.knn as cl
+import utilities.plotting
+from matplotlib import pyplot as plt
 
-classifiers.knn.test_1nn()
+(confusion_matrix, misclassified, correctly_classified) = cl.classifier_1nn()
+print(confusion_matrix)
+
+utilities.plotting.draw_images(misclassified)
+utilities.plotting.draw_images(correctly_classified)
+plt.show()
