efaf3fe8ede7b4230817e3a043686f1c3197406b
Finn Ferdinand Sandvand <ffsandva@stud.ntnu.no>
2023-04-13 14:08:22 +0200

    iterate through chunks of training set

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import scipy.io
 
 # Import dataset
-ALL_DATA_PATH = 'MNist_ttt4275/data_all.mat'
-data_all = scipy.io.loadmat(ALL_DATA_PATH)
+DATA_ALL_PATH = 'MNist_ttt4275/data_all.mat'
+data_all = scipy.io.loadmat(DATA_ALL_PATH)
+trainv = data_all['trainv'] # training data
+testv = data_all['testv']   # testing data
+
+num_train = data_all['num_train']
+num_test = data_all['num_test']
+row_size = data_all['row_size']
+col_size = data_all['col_size']
+vec_size = data_all['vec_size']
+
+# Iterating through chunks of CHUNK_SIZE images from the training set
+CHUNK_SIZE = 1000
+n_chunks = num_train // CHUNK_SIZE
+print(n_chunks)
+for chunk in range(n_chunks):
+    chunk_data = trainv[CHUNK_SIZE*chunk : CHUNK_SIZE*(chunk+1), vec_size]
+    # print(chunk_data.shape)
